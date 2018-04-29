e92703c0a61ef8dd772b2615a1aeb616b8175a4c
Haoliang Yu <hyu@esri.com>
2018-04-29 04:55:21 -0400

    add truffle config

--- truffle.js
+++ truffle.js
@@ -5,7 +5,13 @@ module.exports = {
       host: "localhost",
       port: 7545,
       network_id: 5777 // Match any network id
-    }
+    },
+    ropsten:  {
+     network_id: 3,
+     host: "localhost",
+     port:  8545,
+     gas:   2900000
+   }
   },
   solc: {
     optimizer: {
