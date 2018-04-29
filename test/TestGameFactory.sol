56cf84858f0a709e1fba873f6b50785cc8cead7d
Patrick <Blockchainbuddha@gmail.com>
2018-04-28 20:22:35 -0400

    Testing the waters

--- test/TestGameFactory.sol
+++ test/TestGameFactory.sol
@@ -2,15 +2,24 @@ pragma solidity ^0.4.17;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/Game.sol";
+import "../contracts/GameFactory.sol";
 
-contract TestAdoption {
-  Game game = Adoption(DeployedAddresses.Game());
+contract TestGameFactory {
+//  GameFactory factory = GameFactory(DeployedAddresses.GameFactory());
   // Testing the adopt() function
-  function testStartGame() public {
-    game.startGame();
-    Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
+  function testCreateGame(){
+    GameFactory factory = GameFactory(DeployedAddresses.GameFactory());
+    factory.createGame(1,"Q1;Q2;Q3;","A1;A2;A3;");
+    address gameAddress = factory.contracts(0);
+    Assert.isNotZero(gameAddress,"createGame() should deploy a new contract");
   }
+
+  function testGetContractOwner() {
+    GameFactory factory = GameFactory(DeployedAddresses.GameFactory());
+    address expected = this;
+    Assert.equal(factory.owner(), expected, "GameFactory owner should be equal to this wallet address.");
+  }
+}
   // Testing retrieval of a single pet's owner
   // function testGetAdopterAddressByPetId() public {
   //   // Expected owner is this contract
@@ -30,4 +39,3 @@ contract TestAdoption {
 
   //   Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recorded.");
   // }
-}
