b34753d628bc4eb7bc675d9e02c84b40ab9d18f7
Alex Leone <alex@leone.tech>
2018-04-29 07:26:32 -0400

    Styled main start

--- src/MainStart.js
+++ src/MainStart.js
@@ -6,12 +6,9 @@ class MainStart extends Component {
     return (
       <div className='container game-all'>
         <div className='row'>
-          <div className='col-md-12'>
-            <p>HQ On the BlockChain</p>
-            <Link className='block-link' to='/create'>Create</Link>
-            <Link className='block-link' to='/manage'>Manage</Link>
-            <Link className='block-link' to='/play'>Play</Link>
-
+          <div className='col-md-12' style={{textAlign: 'center'}}>
+            <h1>HQ On the BlockChain</h1>
+            <h3><Link className='block-link' to='/create'>Create A New Game</Link></h3>
           </div>
         </div>
       </div>
